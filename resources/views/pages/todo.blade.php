97c4220702c1544ae05e0a8e7aaf8a9e061de7cc
Andyka Putra <andykaputra219@gmail.com>
2024-02-26 10:58:24 +0700

    ganti submit

--- resources/views/pages/todo.blade.php
+++ resources/views/pages/todo.blade.php
@@ -558,7 +558,7 @@
                             <div class="flex justify-center col-span-2">
                                 <button type="submit" id="edit_todo_btn"
                                     class="text-white w-1/2 items-center bg-primary hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-primary dark:focus:ring-red-800">
-                                    Tambahkan
+                                    Submit
                                 </button>
                             </div>
                         </form>
